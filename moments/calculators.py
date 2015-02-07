4bd6b3ea27b89e6c450119b4fe8d9c70d1b0d6cc
Rıdvan Örsvuran <flasherdn@gmail.com>
2015-02-07 14:29:24 +0200

    ScalarMomentCalc is a subclass of DiscreteMomentCalc
    To do this first we discretize the function, then use methods on
    DiscreteMomentCalc to calculate moments.

--- moments/calculators.py
+++ moments/calculators.py
@@ -8,7 +8,111 @@ from moments.utils import animate_1d_function
 from moments.utils import animate_2d_function
 
 
-class ScalarMomentCalc(object):
+class DiscreteScalarMomentCalc(object):
+    def __init__(self, values, points, times, dxs, dt):
+        """Points in time and space
+        """
+        self.dimension = len(dxs)
+        self.values = values
+        self.points = points
+        self.times = times
+        self.dxs = dxs
+
+        # Multiply all dx values to find dV
+        self.dv = 1
+        for dx in self.dxs:
+            self.dv = self.dv*dx
+
+        self.dt = dt
+        self.m0 = None
+
+    def moment(self, q, tau, m=0, n=0, ks=[]):
+        """Calculates and returns moment of the function.
+
+        Args:
+            q (list): point to calculate moment on ([x, y, z]).
+            tau (float): time to calculate moment on.
+            m (int): spatial degree of moment.
+            n (int): temporal degree of moment.
+            ks (list): spatial components used to calculate moment.
+              Values for k are 0, 1 and 2 for x, y and z, respectively.
+              For example, to calculate
+              moment on x and z ks should be ks = [0, 2]
+        Returns:
+            float: Calculated moment value
+        """
+        vs = self.values*self.dv*self.dt
+        for k in ks:
+            vs *= (self.points[:, k] - q[k])
+        for j in range(n):
+            vs *= (self.times - tau)
+        return vs.sum()
+
+    def moment0(self, recalc=False):
+        """Shortcut to calculate zero order moment
+
+        Args:
+            recalc (bool, optional): calculate the value even if it is
+              calcuted before. Otherwise, it will return the cached value.
+
+        Returns:
+            float: value of zero order moment
+        """
+        if self.m0 and not recalc:
+            return self.m0
+        self.m0 = self.moment(np.zeros(self.dimension), 0, m=0, n=0)
+        return self.m0
+
+    def moment_all(self, q, tau, m=0, n=0):
+        """Calculates moment for all spatial component combination.
+
+        Works for m <= 2. For m = 1 it calculates moment for all
+        components and returns a vector. For m = 2, it calculates
+        moment for all two component combinations and returns a
+        matrix.
+
+        Args:
+            q (list): point to calculate moment on ([x, y, z])
+            tau (float): time to calculate moment on
+            m (int): spatial degree of moment. Should be less than 3.
+            n (int): temporal degree of moment
+
+        Returns:
+            numpy.ndarray: moment values. For m = 1 it is a vector, for
+                           m =  2 it is a matrix.
+
+        """
+        # if m is equals to 0, there is only one moment
+        if m == 0:
+            return self.moment(q, tau, m, n)
+        # m > 2 case requires unneeded complexity
+        if m > 2:
+            raise NotImplemented
+        indices = range(self.dimension)
+        # shape of combinations
+        # m = 1 => a dimension length vector
+        # m = 2 => a matrix dimension by dimension
+        shape = np.ones(m)*self.dimension
+        moments = np.zeros(shape)
+        for ks in itertools.combinations_with_replacement(indices, m):
+            moment = self.moment(q, tau, m, n, ks)
+            moments[ks] = moment
+            if m == 2:  # Mij = Mji
+                moments[tuple(reversed(ks))] = moment
+        return moments
+
+    def center_of_gravity(self):
+        """Calculates and returns center of gravity.
+
+        It is calculated by dividing first order moment at origin by
+        zero order moment.
+        """
+        self.m0 = self.moment0()
+        m1 = self.moment_all(np.zeros(self.dimension), 0, m=1, n=0)
+        return m1/self.m0
+
+
+class ScalarMomentCalc(DiscreteScalarMomentCalc):
     """Moment Calculator for scalar valued functions
 
     Args:
@@ -91,121 +195,27 @@ class ScalarMomentCalc(object):
     """
     def __init__(self,  function, space_intervals, dxs, domain,
                  time_interval=[1, 1], dt=1):
-        self.function = function
-        self.dxs = dxs
-
-        # multiply dx terms to find dv
-        self.dv = 1
-        for dx in self.dxs:
-            self.dv *= dx
-
-        self.dt = dt
         self.points = grid(space_intervals, dxs, domain)
-        self.times = crange(time_interval[0], time_interval[1], self.dt)
-        self.dimension = len(self.dxs)
-        self.m0 = None
-        self.values = None
+        self.times = crange(time_interval[0], time_interval[1], dt)
+        self.dimension = len(dxs)
+        self.function = function
+        values, args = self._discretize()
+        super(ScalarMomentCalc, self).__init__(values,
+                                               args['points'],
+                                               args['times'],
+                                               dxs, dt)
 
-    def _calc_values(self, recalc=False):
-        if self.values is not None and not recalc:
-            return (self.values, self.args)
-        self.values = []
-        self.args = np.array([(p, t) for t in self.times
-                              for p in self.points],
-                             dtype=[('points', np.float64, (self.dimension,)),
-                                    ('times', np.float64)])
-        for point, time in self.args:
-            self.values.append(self.function(point, time))
-        self.values = np.array(self.values)
-        return (self.values, self.args)
-
-    def moment(self, q, tau, m=0, n=0, ks=[]):
-        """Calculates and returns moment of the function.
-
-        Args:
-            q (list): point to calculate moment on ([x, y, z]).
-            tau (float): time to calculate moment on.
-            m (int): spatial degree of moment.
-            n (int): temporal degree of moment.
-            ks (list): spatial components used to calculate moment.
-              Values for k are 0, 1 and 2 for x, y and z, respectively.
-              For example, to calculate
-              moment on x and z ks should be ks = [0, 2]
-        Returns:
-            float: Calculated moment value
-        """
-        self.values, self.args = self._calc_values()
-        ps = self.args['points']
-        ts = self.args['times']
-        vs = self.values*self.dv*self.dt
-        for k in ks:
-            vs *= (ps[:, k] - q[k])
-        for j in range(n):
-            vs *= (ts - tau)
-        return vs.sum()
-
-    def moment0(self, recalc=False):
-        """Shortcut to calculate zero order moment
-
-        Args:
-            recalc (bool, optional): calculate the value even if it is
-              calcuted before. Otherwise, it will return the cached value.
-
-        Returns:
-            float: value of zero order moment
-        """
-        if self.m0 and not recalc:
-            return self.m0
-        self.m0 = self.moment(np.zeros(self.dimension), 0, m=0, n=0)
-        return self.m0
-
-    def moment_all(self, q, tau, m=0, n=0):
-        """Calculates moment for all spatial component combination.
-
-        Works for m <= 2. For m = 1 it calculates moment for all
-        components and returns a vector. For m = 2, it calculates
-        moment for all two component combinations and returns a
-        matrix.
-
-        Args:
-            q (list): point to calculate moment on ([x, y, z])
-            tau (float): time to calculate moment on
-            m (int): spatial degree of moment. Should be less than 3.
-            n (int): temporal degree of moment
-
-        Returns:
-            numpy.ndarray: moment values. For m = 1 it is a vector, for
-                           m =  2 it is a matrix.
-
-        """
-        # if m is equals to 0, there is only one moment
-        if m == 0:
-            return self.moment(q, tau, m, n)
-        # m > 2 case requires unneeded complexity
-        if m > 2:
-            raise NotImplemented
-        indices = range(self.dimension)
-        # shape of combinations
-        # m = 1 => a dimension length vector
-        # m = 2 => a matrix dimension by dimension
-        shape = np.ones(m)*self.dimension
-        moments = np.zeros(shape)
-        for ks in itertools.combinations_with_replacement(indices, m):
-            moment = self.moment(q, tau, m, n, ks)
-            moments[ks] = moment
-            if m == 2:  # Mij = Mji
-                moments[tuple(reversed(ks))] = moment
-        return moments
-
-    def center_of_gravity(self):
-        """Calculates and returns center of gravity.
-
-        It is calculated by dividing first order moment at origin by
-        zero order moment.
-        """
-        self.m0 = self.moment0()
-        m1 = self.moment_all(np.zeros(self.dimension), 0, m=1, n=0)
-        return m1/self.m0
+    def _discretize(self):
+        values = []
+        args = np.array([(p, t) for t in self.times
+                         for p in self.points],
+                        dtype=[('points', np.float64,
+                                (self.dimension,)),
+                               ('times', np.float64)])
+        for point, time in args:
+            values.append(self.function(point, time))
+        values = np.array(values)
+        return (values, args)
 
     def animate_function(self):
         """Animates 1D and 2D time based functions."""
