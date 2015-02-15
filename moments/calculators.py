b6306857d99a0dd6133efe6a206cec22cf6bb4cc
Rıdvan Örsvuran <flasherdn@gmail.com>
2015-02-15 13:15:42 +0200

    added documentation for TensorMomentCalc and DiscreteScalarMomentCalc

--- moments/calculators.py
+++ moments/calculators.py
@@ -9,9 +9,37 @@ from moments.utils import animate_2d_function
 
 
 class DiscreteScalarMomentCalc(object):
+    """Moment calculater discrete scalar distributions
+
+    Args:
+        values (numpy.ndarray): discretely distributed scalar values
+        points (numpy.ndarray): points which values belong
+        times (numpy.ndarray): time which value belong
+        dxs (list): [dx, dy, dz] values
+        dt (float): dt value
+
+    Examples:
+
+        Value index should point to correspoding point and time
+        values.
+
+        An example for points, times and values:
+        |--------+--------+------|
+        | Values | Points | Time |
+        |--------+--------+------|
+        |      0 | (0, 0) |    0 |
+        |      1 | (0, 1) |    0 |
+        |      2 | (0, 2) |    0 |
+        |      3 | (0, 0) |    1 |
+        |      4 | (0, 1) |    1 |
+        |      5 | (0, 2) |    1 |
+        |      6 | (0, 0) |    2 |
+        |      7 | (0, 1) |    2 |
+        |      8 | (0, 2) |    2 |
+        |--------+--------+------|
+
+    """
     def __init__(self, values, points, times, dxs, dt):
-        """Points in time and space
-        """
         self.dimension = len(dxs)
         self.values = values
         self.points = points
@@ -52,11 +80,14 @@ class DiscreteScalarMomentCalc(object):
         """Shortcut to calculate zero order moment
 
         Args:
+
             recalc (bool, optional): calculate the value even if it is
-              calcuted before. Otherwise, it will return the cached value.
+              calcuted before. Otherwise, it will return the cached
+              value.
 
         Returns:
             float: value of zero order moment
+
         """
         if self.m0 and not recalc:
             return self.m0
@@ -200,13 +231,25 @@ class ScalarMomentCalc(DiscreteScalarMomentCalc):
         self.dimension = len(dxs)
         self.function = function
         values, args = self._discretize()
+        # After discretization, we can use DiscreteScalarMomentCalc
+        # operations.
         super(ScalarMomentCalc, self).__init__(values,
                                                args['points'],
                                                args['times'],
                                                dxs, dt)
 
     def _discretize(self):
+        # This will sample function at discrete point and time.
         values = []
+        # Find combination of points and time.
+        # For example, Turn
+        #  times = [0, 1, 2]
+        #  points = [(0, 0), (1, 1), (2, 2)]
+        # into
+        #  times = [0, 0, 0, 1, 1, 1, 2, 2]
+        #  points = [(0, 0), (1, 1), (2, 2),
+        #            (0, 0), (1, 1), (2, 2),
+        #            (0, 0), (1, 1), (2, 2)]
         args = np.array([(p, t) for t in self.times
                          for p in self.points],
                         dtype=[('points', np.float64,
@@ -228,6 +271,15 @@ class ScalarMomentCalc(DiscreteScalarMomentCalc):
 
 
 class TensorMomentCalc(DiscreteScalarMomentCalc):
+    """Moment calculator for second rank tensor distributions
+
+    Args:
+        values (numpy.ndarray): discretely distributed tensor values
+        points (numpy.ndarray): points which values belong
+        times (numpy.ndarray): times which values belong
+        dxs (list): [dx, dy, dz] values
+        dt (float): dt value
+    """
     def __init__(self, values, points, times, dxs, dt):
 
         self.real_values = values
@@ -238,12 +290,33 @@ class TensorMomentCalc(DiscreteScalarMomentCalc):
         self.dxs = dxs
         self.dt = dt
         projected_values = self._project_values_to_m0()
+        # projected_values is a scalar distribution. So, we can use
+        # DiscreteScalarMomentCalc operations.
         super(TensorMomentCalc, self).__init__(projected_values,
                                                points,
                                                times,
                                                dxs, dt)
 
     def tensor_moment0(self, recalc=False):
+        """Returns zero order moment for tensor distribution.
+
+        It is calculated by treating each component of tensors as
+        scalar distribution. Resulting tensor's components are
+        calculated by calculating zero order moment for each
+        component distribution.
+
+        Args:
+
+            recalc (bool, optional): calculate the value even if it is
+              calcuted before. Otherwise, it will return the cached
+              value.
+
+        Returns:
+
+            (numpy.ndarray): tensor zero order moment which has same
+            shape as input values
+
+        """
         if self.tensor_m0 is not None and not recalc:
             return self.tensor_m0
 
@@ -251,6 +324,10 @@ class TensorMomentCalc(DiscreteScalarMomentCalc):
         # for each element find m0
         for i in range(self.shape[0]):
             for j in range(self.shape[1]):
+                # Treat each component distribution as scalar
+                # distribution.
+                # self.real_values[:, 1, 1] will return (1, 1)
+                # component of all tensors in the distribution.
                 calc = DiscreteScalarMomentCalc(self.real_values[:, i, j],
                                                 self.points,
                                                 self.times,
@@ -260,9 +337,15 @@ class TensorMomentCalc(DiscreteScalarMomentCalc):
         return self.tensor_m0
 
     def _project_values_to_m0(self):
+        # If tensor_m0 is not calculated before, calculate it first.
         if self.tensor_m0 is None:
             self.tensor_m0 = self.tensor_moment0()
 
+        # This operation will dot product all tensor values with the
+        # zero order moment. Which means sum of element-wise
+        # multiplication. Thus, this will turn tensor distribution
+        # into a scalar distribution. We can think this operation as
+        # projection onto zero order moment.
         projected_values = np.tensordot(self.real_values,
                                         self.tensor_m0)
 
