6a90d5a5a8140b39bf2a0d828509be711258710f
Rıdvan Örsvuran <flasherdn@gmail.com>
2015-07-26 13:13:10 +0300

    added find_derivative
    This script calculates green's function's spatial derivative from
    moments of the distribution.
    
    Also added utility functions to switch between math notation to filename
    notation for green's functions.

--- greens/reader.py
+++ greens/reader.py
@@ -4,7 +4,8 @@ import os
 
 from greens.utils import g_to_h
 from greens.utils import derivative
-
+from greens.utils import parse_greens_name
+from greens.utils import get_greens_filename
 from obspy.core import read
 
 
@@ -62,3 +63,30 @@ class ReaderForce(object):
     def get_h(self, n, k, spatial='', temporal=0):
         g = self.get(n, k, spatial, temporal)
         return g_to_h(g)
+
+
+class ReaderNew(object):
+
+    def __init__(self, folder, station_name):
+        self.folder = os.path.join(folder,
+                                   station_name+"_greens")
+        self.name = station_name
+
+    def get(self, greens_name):
+        """Get green's function from filename
+
+        Args:
+            greens_name (str): dt^n (G|H)_nk,lij
+
+        Returns:
+            obspy.core.stream.Stream: green's function
+        """
+        filename = get_greens_filename(greens_name,
+                                       self.name)+"i"
+        file_data = parse_greens_name(greens_name)
+        filepath = os.path.join(self.folder, filename)
+        st = read(filepath)
+        if file_data['is_h']:
+            st = g_to_h()
+
+        return derivative(st, file_data['n'])
--- greens/utils.py
+++ greens/utils.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import re
 
 def g_to_h(st):
     """Converts G to H
@@ -18,3 +18,71 @@ def derivative(st, n):
     for i in range(n):
         tr.differentiate()
     return st
+
+
+def get_greens_name(m, n, component, terms, for_force=False):
+    """Return corresponding green's function for moment
+
+    Args:
+        moment_index (tuple): ((m,n), (indices))
+        m (int): spatial moment
+        n (int): temporal moment
+        component (int): component (0=x, 1=y, 2=z, 3=n)
+        terms (numpy.ndarray): other terms
+        for_force (bool): use force formulation instead of moment tensor
+
+    Returns:
+        str: Green's function's name
+    """
+    c = "xyzn"
+    # for force formulation use G, for moment tensor use H
+    f_name = "G" if for_force else "H"
+    comp = c[component]
+    subs = "".join([c[term] for term in terms])
+    name = "{G}_{comp}{subs}".format(G=f_name,
+                                     comp=comp,
+                                     subs=subs)
+
+    # if name has more than 2 subscripts add a comma
+    if len(name) > 4:
+        name = name[:4] + "," + name[4:]
+
+    # if there is a time derivative
+    dt = "dt" if for_force else "dT"
+    if n == 1:
+        name = "{dt} ".format(dt=dt) + name
+    if n > 1:
+        name = "{dt}^{n} ".format(dt=dt, n=n) + name
+
+    return name
+
+
+def parse_greens_name(greens_name):
+    parser = re.compile('(d[tT](\^(?P<n>\d+))? )?(?P<name>\w)_(?P<comp>\w)(?P<terms>[\w,]*)')
+    match = parser.match(greens_name)
+    n = match.group("n") or 0
+    # special case for dt G..., n => 1
+    if n == 0 and match.group(1) is not None:
+        n = 1
+    is_h = match.group("name") == "H"
+    comp = match.group("comp")
+    terms = match.group("terms").replace(",", "")
+    zero_terms = 2 if is_h else 1
+    m = len(terms) - zero_terms
+    data = {
+        "m": m,
+        "n": n,
+        "is_h": is_h,
+        "comp": comp,
+        "terms": terms,
+    }
+    return data
+
+
+def get_greens_filename(greens_name, station_name):
+    """Return green's function's filename
+    """
+    data = parse_greens_name(greens_name)
+    return "{name}_{terms}.{comp}".format(name=station_name,
+                                          terms=data['terms'],
+                                          comp=data['comp'])
